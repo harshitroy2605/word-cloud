985470d16dd8b83470e55d23a1c7378355b7a878
HarshitRoys <harshitroy1402@gmail.com>
2019-11-18 15:16:30 +0530

    project files

---
+++ 1.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+from PIL import Image
+import wikipedia
+from wordcloud import WordCloud,STOPWORDS
+
+x=str(input("enter the title : "))
+title=wikipedia.search(x)[0]
+page=wikipedia.page(title)
+text=page.content
+print(text)
+background=np.array(Image.open("cloud.png"))
+stopwords=set(STOPWORDS)
+wc=WordCloud(background_color="white",
+				max_words=200,
+				mask=background,
+				stopwords=stopwords)
+
+wc.generate(text)
+wc.to_file("1.png")
